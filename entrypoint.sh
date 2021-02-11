2aa5dc48da061f4a9ac514a96022e9a935562732
Christian Drüke <christian@drueke.biz>
2021-02-11 19:13:38 +0100

    enhance(#2): Use environment variable to forward parameter to wssd.py and samba.sh.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 echo "Start Web Service Discovery host daemon (WSDD)"
-python3 wsdd.py &
+echo "Parameter: ${WSSD_PARAMETER}"
+#python3 wsdd.py ${WSSD_PARAMETER} &
+
 echo ""
 echo "Start /usr/bin/samba.sh from original docker image"
-/usr/bin/samba.sh $@
+
+# If the SAMBA_PARAMETER environment variable is not set use the commandline parameter.
+if [ -z "${SAMBA_PARAMETER}" ]
+then
+      SAMBA_PARAMETER=$@
+fi
+
+echo "Parameter: ${SAMBA_PARAMETER}"
+
+#/usr/bin/samba.sh ${SAMBA_PARAMETER}
